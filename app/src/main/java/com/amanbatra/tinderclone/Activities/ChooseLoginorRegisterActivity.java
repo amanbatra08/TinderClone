72bd5f44740819d0777d31342667807773b46542
amanbatra08 <48562256+amanbatra08@users.noreply.github.com>
2020-07-19 02:11:46 +0530

    nji

---
+++ app/src/main/java/com/amanbatra/tinderclone/Activities/ChooseLoginorRegisterActivity.java
@@ -0,0 +1,45 @@
+package com.amanbatra.tinderclone.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.amanbatra.tinderclone.R;
+
+public class ChooseLoginorRegisterActivity extends AppCompatActivity {
+
+    private Button mLogin, mRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_loginor_register);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseLoginorRegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChooseLoginorRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+    }
+}
